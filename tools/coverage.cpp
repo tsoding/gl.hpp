ddb4c0857edd3141d8942ce343c18929c3fcd0a4
rexim <reximkut@gmail.com>
2020-07-21 18:48:24 +0700

    [tools] Start using aids for coverage
    https://github.com/rexim/aids

--- tools/coverage.cpp
+++ tools/coverage.cpp
@@ -1,96 +1,6 @@
-#include <cassert>
-#include <cstdlib>
-#include <cstdio>
-#include <cctype>
-#include <cstring>
+#include "aids.hpp"
 
-struct String_View
-{
-    const char *data;
-    size_t count;
-
-    void chop_left(size_t n)
-    {
-        if (n <= count) {
-            data += n;
-            count -= n;
-        }
-    }
-
-    void chop_right(size_t n)
-    {
-        if (n <= count) {
-            count -= n;
-        }
-    }
-
-    void grow(size_t n)
-    {
-        count += n;
-    }
-
-    String_View chop_by_delim(char delim)
-    {
-        String_View result = {};
-        result.data = data;
-
-        while (count > 0 && *data != delim) {
-            chop_left(1);
-            result.grow(1);
-        }
-
-        if (count > 0 && *data == delim) {
-            chop_left(1);
-        }
-
-        return result;
-    }
-
-    void println(FILE *stream)
-    {
-        fwrite(data, 1, count, stream);
-        putc('\n', stream);
-    }
-
-    String_View trim_left() const
-    {
-        String_View result = *this;
-        while (result.count > 0 && isspace(*result.data)) {
-            result.chop_left(1);
-        }
-        return result;
-    }
-
-    String_View trim_right() const
-    {
-        String_View result = *this;
-        while (result.count > 0 && isspace(*(result.data + result.count - 1))) {
-            result.chop_right(1);
-        }
-        return result;
-    }
-
-    String_View trim() const
-    {
-        return trim_left().trim_right();
-    };
-
-    bool operator==(const String_View &that) const
-    {
-        if (this->count != that.count) return false;
-        return memcmp(this->data, that.data, this->count) == 0;
-    }
-
-    bool operator!=(const String_View &that) const
-    {
-        return !(*this == that);
-    }
-};
-
-String_View operator ""_sv(const char *data, size_t count)
-{
-    return {data, count};
-}
+using namespace aids;
 
 struct String_Buffer
 {
@@ -109,15 +19,9 @@ struct String_Buffer
         data[count++] = x;
     }
 
-    void println(FILE *stream)
-    {
-        fwrite(data, 1, count, stream);
-        putc('\n', stream);
-    }
-
     String_View to_view()
     {
-        return {data, count};
+        return {count, data};
     }
 
     char *end()
@@ -126,53 +30,29 @@ struct String_Buffer
     }
 };
 
-String_View read_whole_file(const char *file_path)
-{
-    FILE *f = fopen(file_path, "rb");
-    assert(f);
-
-    fseek(f, 0, SEEK_END);
-    long m = ftell(f);
-    char *result = (char *) malloc(m);
-    assert(result);
-    fseek(f, 0, SEEK_SET);
-    fread(result, 1, m, f);
-
-    fclose(f);
-
-    return {result, (size_t) m};
-}
-
 String_View chop_csv_field(String_View *line, char delim, char esc,
                            String_Buffer *buffer)
 {
-    String_View field = {buffer->end(), 0};
+    String_View field = {0, buffer->end()};
 
     while (line->count > 0) {
         if (*line->data == delim) {
-            line->chop_left(1);
+            line->chop(1);
             return field;
         } else if (*line->data == esc && line->count > 1) {
-            line->chop_left(1);
+            line->chop(1);
             buffer->push(*line->data);
             field.grow(1);
-            line->chop_left(1);
+            line->chop(1);
         } else {
             buffer->push(*line->data);
             field.grow(1);
-            line->chop_left(1);
+            line->chop(1);
         }
     }
 
     return field;
 }
-
-template <typename T>
-struct Maybe
-{
-    bool has_value;
-    T unwrap;
-};
 
 struct Csv
 {
@@ -203,81 +83,135 @@ struct Csv
 
 Csv csv_from_file(const char *file_path)
 {
+    auto file_content = read_file_as_string_view(file_path);
+    assert(file_content.has_value);
+
     Csv csv = {};
-    csv.input = read_whole_file(file_path);
+    csv.input = file_content.unwrap;
     csv.buffer.data = (char*) malloc(csv.input.count);
+    assert(csv.buffer.data);
     csv.buffer.capacity = csv.input.count;
     return csv;
 }
 
 void usage(FILE *stream)
 {
-    fprintf(stream,
-            "Usage: ./coverage <subcommand> <todo-file>\n"
+    println(stream,
+            "Usage: ./coverage <subcommand> <subcommand-args>\n"
             "Subcommands:\n"
-            "    summary      Print the summary of the implementation coverage\n"
-            "    done         Print the list of implemented functions\n"
-            "    todo         Print the list of not implemented functions\n");
+            "    summary <todo-filepath>          Print the summary of the implementation coverage\n"
+            "    done <todo-filepath>             Print the list of implemented functions\n"
+            "    todo <todo-filepath>             Print the list of not implemented functions\n");
 }
 
-String_View string_view_from_cstr(const char *cstr)
+void summary_subcommand(const char *file_path)
 {
-    return {cstr, strlen(cstr)};
+    Csv csv = csv_from_file(file_path);
+
+    size_t implemented = 0;
+    size_t total = 0;
+
+    while (csv.next_row()) {
+        auto field = csv.next_field();
+        if (field.unwrap == "+"_sv) implemented += 1;
+        total += 1;
+    }
+
+    printf("Implemented: %ld/%ld (%.2f%%)\n",
+           implemented, total,
+           total > 0 ? (float) implemented / (float) total * 100.0 : 100.0);
 }
+
+void done_subcommand(const char *file_path)
+{
+    Csv csv = csv_from_file(file_path);
+
+    while (csv.next_row()) {
+        auto implemented = csv.next_field();
+        auto name = csv.next_field();
+        assert(implemented.has_value);
+        assert(name.has_value);
+
+        if (implemented.unwrap == "+"_sv) {
+            println(stdout, name);
+        }
+    }
+}
+
+void todo_subcommand(const char *file_path)
+{
+    Csv csv = csv_from_file(file_path);
+
+    while (csv.next_row()) {
+        auto implemented = csv.next_field();
+        auto name = csv.next_field();
+        assert(implemented.has_value);
+        assert(name.has_value);
+
+        if (implemented.unwrap != "+"_sv) {
+            println(stdout, name);
+        }
+    }
+}
+
+struct Args
+{
+    int argc;
+    char **argv;
+
+    bool empty()
+    {
+        return argc == 0;
+    }
+
+    const char *pop()
+    {
+        assert(!empty());
+        const char *result = *argv;
+        argc -= 1;
+        argv += 1;
+        return result;
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
+    Args args = {argc, argv};
+    args.pop();
+
+    if (args.empty()) {
+        println(stderr, "[ERROR] subcommand is not provided\n");
         usage(stderr);
         return -1;
     }
 
-    String_View subcommand = string_view_from_cstr(argv[1]);
-    const char *file_path = argv[2];
+    String_View subcommand = cstr_as_string_view(args.pop());
 
     if (subcommand == "summary"_sv) {
-        Csv csv = csv_from_file(file_path);
-
-        size_t implemented = 0;
-        size_t total = 0;
-
-        while (csv.next_row()) {
-            auto field = csv.next_field();
-            if (field.unwrap == "+"_sv) implemented += 1;
-            total += 1;
+        if (args.empty()) {
+            println(stderr, "[ERROR] filepath is not provided for todo subcommand\n");
+            usage(stderr);
+            return -1;
         }
 
-        printf("Implemented: %ld/%ld (%.2f%%)\n",
-               implemented, total,
-               total > 0 ? (float) implemented / (float) total * 100.0 : 100.0);
+        summary_subcommand(args.pop());
     } else if (subcommand == "done"_sv) {
-        Csv csv = csv_from_file(file_path);
-
-        while (csv.next_row()) {
-            auto implemented = csv.next_field();
-            auto name = csv.next_field();
-            assert(implemented.has_value);
-            assert(name.has_value);
-
-            if (implemented.unwrap == "+"_sv) {
-                name.unwrap.println(stdout);
-            }
+        if (args.empty()) {
+            println(stderr, "[ERROR] filepath is not provided for done subcommand\n");
+            usage(stderr);
+            return -1;
         }
+
+        done_subcommand(args.pop());
     } else if (subcommand == "todo"_sv) {
-        Csv csv = csv_from_file(file_path);
-
-        while (csv.next_row()) {
-            auto implemented = csv.next_field();
-            auto name = csv.next_field();
-            assert(implemented.has_value);
-            assert(name.has_value);
-
-            if (implemented.unwrap != "+"_sv) {
-                name.unwrap.println(stdout);
-            }
+        if (args.empty()) {
+            println(stderr, "[ERROR] filepath is not provided for todo subcommand\n");
+            usage(stderr);
+            return -1;
         }
-    }
 
+        todo_subcommand(args.pop());
+    }
 
     return 0;
 }
