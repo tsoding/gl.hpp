4c225244dc44e3dd2cc9b3cdcbcaf1d55cbab31f
rexim <reximkut@gmail.com>
2020-08-12 02:04:46 +0700

    Make enums subcommand generate enums from the newer source

--- tools/spec.cpp
+++ tools/spec.cpp
@@ -1,3 +1,5 @@
+#include <set>
+#include <map>
 #include "aids.hpp"
 #include <libxml/parser.h>
 #include <libxml/tree.h>
@@ -35,7 +37,7 @@ Node find_node(Node node, const xmlChar *name)
 }
 
 template <typename Parent>
-auto find_child(Parent parent, const xmlChar *name)
+auto find_first_child(Parent parent, const xmlChar *name)
 {
     auto child = parent->children;
     while (child && xmlStrcmp(child->name, name) != 0) {
@@ -108,10 +110,10 @@ void print_footer(FILE *stream)
     println(stream, "#endif  // GL_HPP_");
 }
 
-void gen_subcommand(xmlDocPtr doc)
+void gen_subcommand(const char *filepath, xmlDocPtr doc)
 {
-    xmlNodePtr groupsNode = find_child(doc->children, "groups"_xml);
-    xmlNodePtr commandsNode = find_child(doc->children, "commands"_xml);
+    xmlNodePtr groupsNode = find_first_child(doc->children, "groups"_xml);
+    xmlNodePtr commandsNode = find_first_child(doc->children, "commands"_xml);
 
     print_header(stdout);
     generate_groups(stdout, groupsNode);
@@ -119,9 +121,9 @@ void gen_subcommand(xmlDocPtr doc)
     print_footer(stdout);
 }
 
-void commentg_subcommand(xmlDocPtr doc)
+void commentg_subcommand(const char *filepath, xmlDocPtr doc)
 {
-    xmlNodePtr groupsNode = find_child(doc->children, "groups"_xml);
+    xmlNodePtr groupsNode = find_first_child(doc->children, "groups"_xml);
     auto iter = groupsNode->children;
     while (iter) {
         if (xmlStrcmp(iter->name, "group"_xml) == 0) {
@@ -133,16 +135,81 @@ void commentg_subcommand(xmlDocPtr doc)
     }
 }
 
+String_View xmlstr_as_string_view(const xmlChar *xmlstr)
+{
+    return cstr_as_string_view((const char *)xmlstr);
+}
+
+void enums_subcommand(const char *filepath, xmlDocPtr doc)
+{
+    std::map<String_View, std::map<String_View, String_View>> groups;
+
+    auto registry = doc->children;
+    auto enums = registry->children;
+    while (enums) {
+        if (xmlStrcmp(enums->name, "enums"_xml) == 0) {
+            auto enoom = enums->children;
+            while (enoom) {
+                if (xmlStrcmp(enoom->name, "enum"_xml) == 0) {
+                    auto group_prop = find_node(enoom->properties, "group"_xml);
+                    auto name_prop = find_node(enoom->properties, "name"_xml);
+                    if (!name_prop) {
+                        println(stdout, filepath, ":", enoom->line, ": enum without name found");
+                        fflush(stdout);
+                        abort();
+                    }
+                    auto name_text = xmlstr_as_string_view(name_prop->children->content);
+                    
+                    auto value_prop = find_node(enoom->properties, "value"_xml);
+                    if (!value_prop) {
+                        println(stdout, filepath, ":", enoom->line, ": enum without value found");
+                        fflush(stdout);
+                        abort();
+                    }
+                    auto value_text = xmlstr_as_string_view(value_prop->children->content);
+
+                    if (group_prop) {
+                        auto group_text = xmlstr_as_string_view(group_prop->children->content);
+                        
+                        while (group_text.count) {
+                            auto group = group_text.chop_by_delim(',');
+                            if (groups[group][name_text].count > 0) {
+                                println(stdout, filepath, ":", enoom->line, " enum value ", name_text, " is redefined");
+                                fflush(stdout);
+                                abort();
+                            }
+                            groups[group][name_text] = value_text;
+                        }
+                    }
+                }
+                enoom = enoom->next;
+            }
+        } 
+        enums = enums->next;
+    }
+
+    print_header(stdout);
+    for (const auto &group : groups) {
+        println(stdout, "enum class ", group.first, " {");
+        for (const auto &enoom: group.second) {
+            println(stdout, "  ", enoom.first.subview(3, enoom.first.count - 3), " = ", enoom.second, ",");
+        }
+        println(stdout, "};");
+    }
+    print_footer(stdout);
+}
+
 struct Subcommand
 {
     String_View name;
-    void (*run)(xmlDocPtr doc);
+    void (*run)(const char *filepath, xmlDocPtr doc);
     String_View help;
 };
 
 Subcommand subcommands[] = {
     {"gen"_sv, gen_subcommand, "Generate the gl.hpp from <spec.xml>"_sv},
     {"commentg"_sv, commentg_subcommand, "Print group names and their comments"_sv},
+    {"enums"_sv, enums_subcommand, "Print all enums"_sv},
 };
 
 void usage(FILE *stream)
@@ -220,7 +287,7 @@ int main(int argc, char *argv[])
 
     for (size_t i = 0; i < ARRAY_LEN(subcommands); ++i) {
         if (subcommands[i].name == cstr_as_string_view(subcommand)) {
-            subcommands[i].run(doc);
+            subcommands[i].run(filepath, doc);
             return 0;
         }
     }
